0a818ddd7bc598fda9f251d5c5a342996e08b599
Nathan Zhang <Nathan.Zhang@softwire.com>
2019-07-01 11:48:37 +0100

    refactoring small things

--- src/Parser.js
+++ src/Parser.js
@@ -26,14 +26,36 @@ export function parse_fml_emdis(raw_string) {
     var emdis_messages = [];
     values_group.forEach(element => {
         var emdis = get_emdis_message(fields, element);
+        emdis.message_type = message_type;
         emdis_messages.push(emdis);
     });
 
     return emdis_messages;
 }
 
+export function compareListOfEmdis(listOne, listTwo) {
+    var result = [];
+    var maxLength = Math.max(listOne.length, listTwo.length);
+    var defaultIfUndefined = (obj) => obj === undefined ? {} : obj;
+    for (var i = 0; i < maxLength; i++) {
+        result.push(compareEmdis(defaultIfUndefined(listOne[i]), defaultIfUndefined(listTwo[i])));
+    }
+    return result;
+}
+
+export function displayListOfDiffs(listDiffs) {
+    var result = "";
+    listDiffs.forEach((element,idx) => {
+        result += `differences in message ${idx} :\n`;
+        result += displayDiff(element);
+        result += "----------------------\n";
+    });
+    return result;
+}
+
 //fields: ["FIELD_1"]
 //values_raw: "raw string"
+//return : {field_1:value1, etc}
 function get_emdis_message(fields, values_raw) {
     var values = values_raw.split(/(?:,|\s|")+/).filter(emptyStringFilter);
     if (values.length > fields.length) {
@@ -60,33 +82,13 @@ function compareEmdis(emdisOne, emdisTwo) {
     return fieldsDiff;
 }
 
-export function compareListOfEmdis(listOne, listTwo) {
-    var result = [];
-    var maxLength = Math.max(listOne.length, listTwo.length);
-    var defaultIfUndefined = (obj) => obj === undefined ? {} : obj;
-    for (var i = 0; i < maxLength; i++) {
-        result.push(compareEmdis(defaultIfUndefined(listOne[i]), defaultIfUndefined(listTwo[i])));
-    }
-    return result;
-}
-
-export function displayListOfDiffs(listDiffs) {
-    var result = "";
-    listDiffs.forEach((element,idx) => {
-        result += `message ${idx} :\n`;
-        result += displayDiff(element);
-        result += "----------------------\n";
-    });
-    return result;
-}
-
 // accept : [{left:string, right: string}]
 function displayDiff(emdisDiff) {
-    var result = Object.keys(emdisDiff).reduce((accu, k) => accu += showEmdisDiff(k, emdisDiff), "");
+    var result = Object.keys(emdisDiff).reduce((accu, k) => accu += showSingleDiff(k, emdisDiff), "");
     return result;
 }
 
-function showEmdisDiff(key, diff) {
+function showSingleDiff(key, diff) {
     var msg = `left: ${diff[key].left}`.padEnd(25, ' ').concat(` \t right: ${diff[key].right} \n`);
     return (`${key}:`.padEnd(15).concat(msg));
 };
